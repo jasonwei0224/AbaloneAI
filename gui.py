c2bac68761938d20f684d8131979e3a47e3a0f70
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-11-02 14:46:11 -0800

    update player score

--- gui.py
+++ gui.py
@@ -1,5 +1,3 @@
-import random
-
 import PySimpleGUI as sg
 
 # List of options for user to choose from
@@ -11,9 +9,9 @@ game_mode = ["human vs human", "human vs computer"]
 config_layout = [[sg.Text('Select Initial Board Layout')],
                  [sg.Radio(board, 1) for board in board_choice],
                  [sg.Text('Select Player Color')],
-                 [sg.Radio(color, 1) for color in player_color],
+                 [sg.Radio(color, 2) for color in player_color],
                  [sg.Text('Select Game Mode')],
-                 [sg.Radio(mode, 1) for mode in game_mode],
+                 [sg.Radio(mode, 3) for mode in game_mode],
                  [sg.Text('Set player 1 (human) move limit per game'),
                   sg.Spin([i for i in range(1, 11)], initial_value=1)],
                  [sg.Text('Set player 2 (computer/human) move limit per game'),
@@ -37,7 +35,7 @@ if event == "Exit":
 elif event == 'Start':
     window.close()  # Close the configuration window
     game_layout = [
-        [sg.Menu(menu, )], [sg.Column([[sg.Text('Game Score: 0 ')]]), sg.Column([[sg.Button('Undo')]])],
+        [sg.Menu(menu, )], [sg.Column([[sg.Text('Player 1: 0   Player2:  0 ')]]), sg.Column([[sg.Button('Undo')]])],
         [sg.Column([[sg.Text('Moves Taken by Player 1:   ')]]),
          sg.Column([[sg.Text('Moves Taken by Player 2: ')]])],
         [sg.Column([[sg.Text('Time Taken by Player 1:   ')]]),
