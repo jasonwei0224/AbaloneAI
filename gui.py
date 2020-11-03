4420d6bfbe3ddc66d198c6b86de12ac53d1e5d5e
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-11-02 22:48:27 -0800

    update redraw function

--- gui.py
+++ gui.py
@@ -1,9 +1,20 @@
 import PySimpleGUI as sg
 
+
 # List of options for user to choose from
 board_choice = ['Standard', 'German Daisy', 'Belgian Daisy']
 player_color = ['Black', 'White']
 game_mode = ["human vs human", "human vs computer"]
+
+
+# based on the board representation given in class change the letter to the row it represents (starting at
+# row 0)
+location_dict = {"I": 0, "H": 1, "G": 2, "F": 3, "E": 4, "D": 5, "C": 6, "B": 7, "A": 8}
+
+# from the row number get the letter and the offset of the number for each location
+letter_and_numOffset = {0: ('I', 5), 1: ('H', 4), 2: ('G', 3), 3: ('F', 2), 4: ('E', 1), 5: ('D', 1), 6: ('C', 1),
+                        7: ('B', 1), 8: ('A', 1)}
+
 
 # Configuration window layout
 config_layout = [[sg.Text('Select Initial Board Layout', text_color="yellow")],
@@ -33,6 +44,18 @@ window = sg.Window('Game Configuration', config_layout, font=('arial', 15))
 
 event, values = window.read()
 
+selected_board = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 2, 2, 0, 0],
+    [2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2],
+]
+
 if event == "Exit":
     window.close()
 elif event == 'Start':
@@ -54,74 +77,28 @@ elif event == 'Start':
         ]
     game_layout = [
         [sg.Graph((600, 600), (0, 300), (300, 0), key='graph'), sg.Column(col)],
-
     ]
+
     window2 = sg.Window('Abalone', game_layout,  font=('arial', 15)).Finalize()  # Create the game window
 
     canvas = window2['graph']
 
-    # default starting position of marbles
-    # default_board = [
-    #     ["-", "-", 1, 1, 1, 1, 1, "-", "-"],
-    #     ["-", 1, 1, 1, 1, 1, 1, "-", "-"],
-    #     ["-", 0, 0, 1, 1, 1, 0, 0, "-"],
-    #     ["-", 0, 0, 0, 0, 0, 0, 0, 0],
-    #     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #     ["-", 0, 0, 0, 0, 0, 0, 0, 0],
-    #     ["-", 0, 0, 2, 2, 2, 0, 0, "-"],
-    #     ["-", 2, 2, 2, 2, 2, 2, "-", "-"],
-    #     ["-", "-", 2, 2, 2, 2, 2, "-", "-"],
-    # ]
-    #
-    # offset_lst = [0, 15, 6, -5, 5, -5, 6, 15, 0]
-    # BOX_SIZE = 28
-    # RADIUS = 13
-    # for row in range(len(default_board)):
-    #     offset = offset_lst[row]
-    #     for col in range(len(default_board[row])):
-    #         if default_board[row][col] == '-':
-    #
-    #             canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color="",
-    #                               line_color='')
-    #
-    #         elif default_board[row][col] == 0:
-    #             canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color="dark grey")
-    #
-    #         elif default_board[row][col] == 1:
-    #             canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='white')
-    #
-    #         elif default_board[row][col] == 2:
-    #             canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='black')
-    #         canvas.draw_text('{}'.format(row * 6 + col + 1),
-    #                     (col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), color="red")
-    default_board = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 2, 2, 2, 0, 0],
-        [2, 2, 2, 2, 2, 2],
-        [2, 2, 2, 2, 2],
-    ]
 
     offset_lst = [48, 38, 28, 17, 5, 17, 28, 38, 48]
     BOX_SIZE = 28
     RADIUS = 13
-    letter_and_numOffset = {0: ('I', 5), 1: ('H', 4), 2: ('G', 3), 3: ('F', 2), 4: ('E', 1), 5: ('D', 1), 6: ('C', 1), 7: ('B', 1), 8: ('A', 1)}
-    for row in range(len(default_board)):
+
+    for row in range(len(selected_board)):
         offset = offset_lst[row]
-        for col in range(len(default_board[row])):
-            if default_board[row][col] == 0:
+        for col in range(len(selected_board[row])):
+            if selected_board[row][col] == 0:
                 canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color="dark grey")
 
-            elif default_board[row][col] == 1:
+            elif selected_board[row][col] == 1:
                 canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='white')
 
-            elif default_board[row][col] == 2:
+            elif selected_board[row][col] == 2:
                 canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='black')
-
 
             canvas.draw_text('{}'.format(letter_and_numOffset[row][0] + str(letter_and_numOffset[row][1]+col)),
                              (col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), color="red")
@@ -129,10 +106,6 @@ elif event == 'Start':
 
     while True:
         event, values = window2.read()
-
-        # based on the board representation given in class change the letter to the row it represents (starting at
-        # row 0)
-        location_dict = {"I": 0, "H": 1, "G": 2, "F": 3, "E": 4, "D": 5, "C": 6, "B": 7, "A": 8}
 
         if event == "Submit":
             move = window2['move'].Get()  # Get the move that user input
@@ -145,20 +118,25 @@ elif event == 'Start':
             end_col = int(move[4]) - end_row
 
             # update the Abalone representation of the board
-            default_board[end_row][end_col + start_row] = default_board[start_row][start_col]
-            default_board[start_row][start_col] = 0
+            selected_board[end_row][end_col + start_row] = selected_board[start_row][start_col]
+            selected_board[start_row][start_col] = 0
 
             # Redraw the board
-            for row in range(len(default_board)):
+            for row in range(len(selected_board)):
                 offset = offset_lst[row]
-                for col in range(len(default_board[row])):
-                    if default_board[row][col] == '-':
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), 10, fill_color="",
-                                          line_color='')
-                    elif default_board[row][col] == 0:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), 10,
+                for col in range(len(selected_board[row])):
+                    if selected_board[row][col] == 0:
+                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
                                           fill_color="dark grey")
-                    elif default_board[row][col] == 1:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), 10, fill_color='white')
-                    elif default_board[row][col] == 2:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), 10, fill_color='black')
+
+                    elif selected_board[row][col] == 1:
+                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
+                                          fill_color='white')
+
+                    elif selected_board[row][col] == 2:
+                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
+                                          fill_color='black')
+
+                    canvas.draw_text(
+                        '{}'.format(letter_and_numOffset[row][0] + str(letter_and_numOffset[row][1] + col)),
+                        (col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), color="red")
