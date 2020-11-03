9ebd000b50283be7a6063957f9689be35219fe08
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-11-02 23:08:38 -0800

    add in board representation for belgin and german daisy

--- gui.py
+++ gui.py
@@ -1,11 +1,10 @@
 import PySimpleGUI as sg
-
+import constant
 
 # List of options for user to choose from
 board_choice = ['Standard', 'German Daisy', 'Belgian Daisy']
 player_color = ['Black', 'White']
 game_mode = ["human vs human", "human vs computer"]
-
 
 # based on the board representation given in class change the letter to the row it represents (starting at
 # row 0)
@@ -15,94 +14,96 @@ location_dict = {"I": 0, "H": 1, "G": 2, "F": 3, "E": 4, "D": 5, "C": 6, "B": 7,
 letter_and_numOffset = {0: ('I', 5), 1: ('H', 4), 2: ('G', 3), 3: ('F', 2), 4: ('E', 1), 5: ('D', 1), 6: ('C', 1),
                         7: ('B', 1), 8: ('A', 1)}
 
-
 # Configuration window layout
 config_layout = [[sg.Text('Select Initial Board Layout', text_color="yellow")],
-                 [sg.Radio(board, 1, default=True) if board == 'Standard' else sg.Radio(board, 1) for board in board_choice],
-                 [sg.VerticalSeparator(pad=((0,0),(10,20)))],
+                 [sg.Radio(board, 1, default=True) if board == 'Standard' else sg.Radio(board, 1) for board in
+                  board_choice],
+                 [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
                  [sg.Text('Select Player Color', text_color="yellow")],
-                 [sg.Radio(color, 2, default=True) if color == 'Black' else sg.Radio(color, 2)  for color in player_color],
-                 [sg.VerticalSeparator(pad=((0,0),(10,20)))],
+                 [sg.Radio(color, 2, default=True) if color == 'Black' else sg.Radio(color, 2) for color in
+                  player_color],
+                 [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
                  [sg.Text('Select Game Mode', text_color="yellow")],
-                 [sg.Radio(mode, 3,  default=True) for mode in game_mode],
-                 [sg.VerticalSeparator(pad=((0,0),(10,20)))],
+                 [sg.Radio(mode, 3, default=True) for mode in game_mode],
+                 [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
                  [sg.Text('Set player move limit per game:'),
                   sg.Spin([i for i in range(1, 11)], initial_value=15)],
-                [sg.VerticalSeparator(pad=((0,0),(10,20)))],
+                 [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
                  [sg.Text('Set player 1 (human) time limit for a move limit (s)'),
                   sg.Spin([i for i in range(1, 11)], initial_value=1)],
-                 [sg.VerticalSeparator(pad=((0,0),(10,20)))],
+                 [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
                  [sg.Text('Set player 2 (computer/human) time limit for a move  (s)'),
                   sg.Spin([i for i in range(1, 11)], initial_value=1)],
-                 [sg.OK("Start", pad=((10,0),(50,10))), sg.Cancel("Exit",  pad=((20,0),(50,10)))]]
+                 [sg.OK("Start", pad=((10, 0), (50, 10))), sg.Cancel("Exit", pad=((20, 0), (50, 10)))]]
 
-# drop down menu layout
-menu = [['Actions', ['Play', 'Stop', 'Pause', 'Reset']],
-        ['History', ['Player Move History', 'Player Time History']]]
+# The layout for the game information including the canvas showing Abalone game board
+game_info_layout = [[sg.Button('Play'), sg.Button('Pause'), sg.Button('Stop'), sg.Button('Undo')],
+                    [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
+                    [sg.InputText("Please Enter your move", key='move'), sg.Button("Submit")],
+                    [sg.Text('Next Move: ....'), ],
+                    [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
+                    [sg.Text('Player 1: 0')],
+                    [sg.Text('Player 2: 0')],
+                    [sg.Text('Time Taken by Player 1:   ')],
+                    [sg.Text('Time Taken by Player 2:  ')],
+                    [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
+                    [sg.Text('Moves Taken by Player 1:   ', pad=((0, 50), (0, 0))),
+                     sg.Text('Moves Taken by Player 2: ')],
+                    [sg.Multiline('Moves Taken by Player 1:   ', size=(25, 10)),
+                     sg.Multiline('Moves Taken by Player 2: ', size=(25, 10))],
+                    ]
+
+# the graph element that the board will be drawn on
+graph_element = sg.Graph((600, 600), (0, 300), (300, 0), key='graph')
+
+
+def draw_board(canvas):
+    '''
+    This draws the Abalone game board with coordinates listed on each location
+    :param canvas: the canavas on the GUI to be draw on
+    :return: null
+    '''
+    offset_lst = [48, 38, 28, 17, 5, 17, 28, 38, 48]  # this changes the distance from the left to the first circle
+    width = 28  # this changes the distance between the marbles
+    radius = 13  # this changes the size of the marble
+    for row in range(len(selected_board)):
+        offset = offset_lst[row]
+        for col in range(len(selected_board[row])):
+            if selected_board[row][col] == 0:
+                canvas.DrawCircle((col * width + 20 + offset, row * width + 15), radius,
+                                  fill_color="dark grey")
+
+            elif selected_board[row][col] == 1:
+                canvas.DrawCircle((col * width + 20 + offset, row * width + 15), radius,
+                                  fill_color='white')
+
+            elif selected_board[row][col] == 2:
+                canvas.DrawCircle((col * width + 20 + offset, row * width + 15), radius,
+                                  fill_color='black')
+
+            canvas.draw_text(
+                '{}'.format(letter_and_numOffset[row][0] + str(letter_and_numOffset[row][1] + col)),
+                (col * width + 20 + offset, row * width + 15), color="red")
+
 
 window = sg.Window('Game Configuration', config_layout, font=('arial', 15))
 
 event, values = window.read()
 
-selected_board = [
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1],
-    [0, 0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 2, 2, 2, 0, 0],
-    [2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2],
-]
+selected_board = constant.DEFAULT_BOARD  # TO_BE_UPDATE: NEED TO CHANGE BASE ON USER SELECTION
 
 if event == "Exit":
     window.close()
 elif event == 'Start':
     window.close()  # Close the configuration window
-    time_col = sg.Col([])
-    col = [[sg.Button('Play'), sg.Button('Pause'), sg.Button('Stop'), sg.Button('Undo')],
-        [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
-        [sg.InputText("Please Enter your move", key='move'), sg.Button("Submit")],
-           [sg.Text('Next Move: ....'), ],
-        [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
-        [sg.Text('Player 1: 0')],
-        [sg.Text('Player 2: 0')],
-        [sg.Text('Time Taken by Player 1:   ')],
-        [sg.Text('Time Taken by Player 2:  ')],
-        [sg.VerticalSeparator(pad=((0, 0), (10, 20)))],
 
-        [sg.Text('Moves Taken by Player 1:   ' ,pad= ((0,50),(0,0))),sg.Text('Moves Taken by Player 2: ')],
-        [sg.Multiline('Moves Taken by Player 1:   ',size=(25,10)), sg.Multiline('Moves Taken by Player 2: ',size=(25,10))],
-        ]
-    game_layout = [
-        [sg.Graph((600, 600), (0, 300), (300, 0), key='graph'), sg.Column(col)],
-    ]
-
-    window2 = sg.Window('Abalone', game_layout,  font=('arial', 15)).Finalize()  # Create the game window
-
+    game_layout = [[graph_element, sg.Column(game_info_layout)]]
+    # Create the game window
+    window2 = sg.Window('Abalone', game_layout, font=('arial', 15)).Finalize()
+    # get the canvas to be drawn on
     canvas = window2['graph']
-
-
-    offset_lst = [48, 38, 28, 17, 5, 17, 28, 38, 48]
-    BOX_SIZE = 28
-    RADIUS = 13
-
-    for row in range(len(selected_board)):
-        offset = offset_lst[row]
-        for col in range(len(selected_board[row])):
-            if selected_board[row][col] == 0:
-                canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color="dark grey")
-
-            elif selected_board[row][col] == 1:
-                canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='white')
-
-            elif selected_board[row][col] == 2:
-                canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS, fill_color='black')
-
-            canvas.draw_text('{}'.format(letter_and_numOffset[row][0] + str(letter_and_numOffset[row][1]+col)),
-                             (col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), color="red")
-
+    # Draw the board
+    draw_board(canvas)
 
     while True:
         event, values = window2.read()
@@ -122,21 +123,4 @@ elif event == 'Start':
             selected_board[start_row][start_col] = 0
 
             # Redraw the board
-            for row in range(len(selected_board)):
-                offset = offset_lst[row]
-                for col in range(len(selected_board[row])):
-                    if selected_board[row][col] == 0:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
-                                          fill_color="dark grey")
-
-                    elif selected_board[row][col] == 1:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
-                                          fill_color='white')
-
-                    elif selected_board[row][col] == 2:
-                        canvas.DrawCircle((col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), RADIUS,
-                                          fill_color='black')
-
-                    canvas.draw_text(
-                        '{}'.format(letter_and_numOffset[row][0] + str(letter_and_numOffset[row][1] + col)),
-                        (col * BOX_SIZE + 20 + offset, row * BOX_SIZE + 15), color="red")
+            draw_board(canvas)
