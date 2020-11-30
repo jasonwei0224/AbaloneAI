191d26ccb132fb77dfd9012dc24ce1eec3c1d873
Vicky <vickychungg@gmail.com>
2020-11-30 10:29:22 -0800

    Adjusted score

--- game_playing_agent.py
+++ game_playing_agent.py
@@ -392,7 +392,8 @@ def eval(state):
                 len(state[5]) * 20 + \
                 calculate_push(state[5])[0] * 10 + \
                 calculate_push(state[5])[1] * 100 + \
-                u * (100) + o * (5)
+                u * (100) + o * (1)
+        print(value)
         return value
 
 
